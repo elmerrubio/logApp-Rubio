8973fbb93a294a2930d320a95ce8601ed7a4e519
elmerrubio <elmerrubiojr25@gmail.com>
2022-03-18 11:03:17 +0800

    p3

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', '');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6479658');
+	define('DB_PASS', '59rLDzfSWM');
+	define('DB_NAME', 'sql6479658');
---
+++ inc/footer.php
@@ -0,0 +1,14 @@
+<footer class="mastfoot mt-auto" style="text-align: center;">
+    <div class="inner">
+      <p>Created By: <br> 
+      <a href="https://github.com/JayrahBautista">JayrahBautista</a>
+      <br>
+      <a href="https://github.com/danmauie">DanMauieDuenas</a>
+      <br>
+      <a href="https://github.com/elmerrubiojr">ElmerRubio</a>
+      <br>BSIT3 - B1
+      </p>
+    </div>
+</footer>
+  </body>
+</html>
